1fdbdc3179a1b7402eee5d38a8f2d44ccd24deb3
Marilia do Valle <mdovallerocha@gmail.com>
2025-04-23 14:38:09 -0300

    Add final check on edit task test

--- cypress/e2e/editar-tarefa.cy.js
+++ cypress/e2e/editar-tarefa.cy.js
@@ -6,5 +6,6 @@ describe('Tarefas', () => {
     it('Editar uma tarefa', () => {
         cy.get(':nth-child(1) > .view > label').dblclick();
         cy.get('.todo-list li.editing .edit').clear().type('Study cypress{enter}');
+        cy.get('.todo-list li').first().should('contain.text', 'Study cypress');
     })
 })
