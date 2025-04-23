7050aaff004547a820a5e0d4254d0e8f8d7be358
Marilia do Valle <mdovallerocha@gmail.com>
2025-04-23 12:56:19 -0300

    Add edit task test

---
+++ cypress/e2e/editar-tarefa.cy.js
@@ -0,0 +1,10 @@
+describe('Tarefas', () => {
+    beforeEach (() => {
+        cy.visit('https://example.cypress.io/todo')
+      })
+
+    it('Editar uma tarefa', () => {
+        cy.get(':nth-child(1) > .view > label').dblclick();
+        cy.get('.todo-list li.editing .edit').clear().type('Study cypress{enter}');
+    })
+})
